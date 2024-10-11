391517bb0758dff22ddc4331e2aeac8e003ceac7
Vaishnavi-kamble77 <vishukamble7703@gmail.com>
2024-10-11 10:44:25 +0530

    Create demo.java

---
+++ demo.java
@@ -0,0 +1,9 @@
+public class program
+{
+  public static void main(String args[])
+  {
+System.out.print("This java program is generated on Github platform");
+System.out.print("This java program is executed using google app engine"); 
+  }
+
+}
