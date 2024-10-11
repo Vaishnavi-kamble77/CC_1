cfe08e1ad7304afd3cc00e12dab43ade36035bc0
Vaishnavi-kamble77 <vishukamble7703@gmail.com>
2024-10-11 10:30:21 +0530

    Create demo.c

---
+++ demo.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+printf("\n\nThis program is generated on github platform\n\n");
+
+printf("\n\nThis C program is execute using Google app engine\n\n");
+return 0;
+
+}
